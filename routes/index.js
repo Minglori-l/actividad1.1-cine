5012c43abed3132f516e326db1f2464ef193e527
Minglori-l <maldonadoperezga@uvm.edu.ve>
2026-05-26 22:58:23 -0400

    Búsqueda por id y creacion de peliculas via POST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const peliculaController = require('../controllers/peliculaController');
+const funcionController = require('../controllers/funcionController');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mi Proyecto de Cine' });
 });
+
+router.get('/cartelera', (req, res) => peliculaController.vistaPeliculas(req, res));
+
+// NUEVA RUTA WEB: Mostrar el formulario (GET)
+router.get('/nueva-pelicula', (req, res) => peliculaController.vistaNuevaPelicula(req, res));
+
+// NUEVA RUTA WEB: Recibir los datos del formulario (POST)
+router.post('/agregar-pelicula', (req, res) => peliculaController.agregarDesdeWeb(req, res));
+
+// NUEVA RUTA WEB: Ver detalles de una película (GET)
+router.get('/pelicula/:id', (req, res) => peliculaController.vistaDetalle(req, res));
+
+router.get('/ver-funciones', (req, res) => funcionController.vistaFunciones(req, res));
 
 module.exports = router;
